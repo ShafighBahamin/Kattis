1c54b9d21b9f456b7b14428862487b744ead084f
Shafigh Bahamin <bahaminshafigh@gmail.com>
2018-04-06 22:16:03 -0600

    Add files via upload

---
+++ Bus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int T = 0;
+        T = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            int num = Int32.Parse(Console.ReadLine());
+            int ans = 1;
+            for (int j = 0; j < num; j++)
+            {
+                ans = ans * 2;
+            }
+            Console.WriteLine(ans-1);
+        }
+    }
+}
